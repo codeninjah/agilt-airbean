9cc4212d943af581b244ae71de98b0c97ea38830
Niklas Andersson <niklas.andersson@iths.se>
2022-02-10 14:56:49 +0100

    Add profile-view and save to current user-function in store

--- airbean/src/store/index.js
+++ airbean/src/store/index.js
@@ -5,10 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    currentUser: {},
   },
   mutations: {
+    saveUser(state, user){
+      state.currentUser = {...user}
+    }
   },
   actions: {
+    save(context, user){
+      context.commit('saveUser', user)
+    }
   },
   modules: {
   }
